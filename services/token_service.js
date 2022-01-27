22f567899ddb68057b5df0b6ba59f7343598a395
kimberlinn99 <alisa.uth99@gmail.com>
2022-01-27 09:07:49 +0700

    fix: datetime to Thailand timezone

--- services/token_service.js
+++ services/token_service.js
@@ -1,52 +1,43 @@
-const connection = require('../config/database')
+const connection = require("../config/database");
 
 export const insertTokenByUserId = async (token, userId) => {
-    let query = 'INSERT INTO Token (Token, Expire, Token_Psn_ID) VALUES '
-    query += '(?, NOW() + INTERVAL 1 HOUR, ?)'
-    
-    try {
-        await connection.promise().execute(
-            query,
-            [token, userId],
-        );
-    } catch (err) {
-        throw new Error(`Insert Token By User Id: ${err.message}`)
-    }
-}
+  let query = "INSERT INTO Token (Token, Expire, Token_Psn_ID) VALUES ";
+  query += "(?, NOW() + INTERVAL 8 HOUR, ?)";
+
+  try {
+    await connection.promise().execute(query, [token, userId]);
+  } catch (err) {
+    throw new Error(`Insert Token By User Id: ${err.message}`);
+  }
+};
 
 export const getTokenByUserId = async (userId) => {
-    let query = 'SELECT ID, Token FROM Token WHERE Token_Psn_ID = ? ;'
-    
-    try {
-        const result = await connection.promise().execute(
-            query,
-            [userId],
-        );
-        return result[0][0]
-    } catch (err) {
-        throw new Error(`Get Token By User Id: ${err.message}`)
-    }
-}
+  let query = "SELECT ID, Token FROM Token WHERE Token_Psn_ID = ? ;";
+
+  try {
+    const result = await connection.promise().execute(query, [userId]);
+    return result[0][0];
+  } catch (err) {
+    throw new Error(`Get Token By User Id: ${err.message}`);
+  }
+};
 
 export const deleteTokenByUserId = async (userId) => {
-    let query = 'DELETE FROM Token WHERE Token_Psn_ID = ? ;'
-    
-    try {
-        await connection.promise().execute(
-            query,
-            [userId],
-        );
-    } catch (err) {
-        throw new Error(`Delete Token By User Id: ${err.message}`)
-    }
-}
+  let query = "DELETE FROM Token WHERE Token_Psn_ID = ? ;";
+
+  try {
+    await connection.promise().execute(query, [userId]);
+  } catch (err) {
+    throw new Error(`Delete Token By User Id: ${err.message}`);
+  }
+};
 
 export const deleteExpiredToken = async () => {
-    let query = 'DELETE FROM Token WHERE Expire < NOW();'
-    
-    try {
-        await connection.promise().execute(query);
-    } catch (err) {
-        throw new Error(`Delete Expired Token: ${err.message}`)
-    }
-}
+  let query = "DELETE FROM Token WHERE Expire < NOW() + INTERVAL 7 HOUR;";
+
+  try {
+    await connection.promise().execute(query);
+  } catch (err) {
+    throw new Error(`Delete Expired Token: ${err.message}`);
+  }
+};
